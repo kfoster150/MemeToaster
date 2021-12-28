3c8ef1146e08a7b4a53253edd51787a4319f73c4
Kenneth F <kfoster150@g.ucla.edu>
2021-12-28 15:02:10 -0800

    cleaning commands - slash commands didn't work

--- launcher.py
+++ launcher.py
@@ -14,7 +14,11 @@ if __name__ == "__main__":
 run like this:
 python -OO launcher.py
 
-invite link:
+invite links
+Admin:
+https://discord.com/api/oauth2/authorize?client_id=920060661294309378&permissions=8&scope=bot%20applications.commands
+
+Individual choices:
 https://discord.com/api/oauth2/authorize?client_id=920060661294309378&permissions=2147609664&scope=bot%20applications.commands
 
 hikari docs:

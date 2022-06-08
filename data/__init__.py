2b0a98668888bab14596cb9d7b623dbd1a2fa17a
Kenneth F <kfoster150@g.ucla.edu>
2022-06-07 21:03:35 -0700

    cursor fetch for getting sql tag list

--- data/__init__.py
+++ data/__init__.py
@@ -1,20 +1,21 @@
 from os import environ
 from boto3 import Session
-from pandas import read_sql
+from pandas import DataFrame
 from psycopg2 import connect
 from urllib.parse import urlparse
 
 def mt_sql_connect():
     url = urlparse(environ['DATABASE_URL'])
 
-    con = connect(
+    conn = connect(
         dbname = url.path[1:],
         user = url.username,
         password = url.password,
         host = url.hostname,
         port = url.port)
 
-    return(con)
+    return(conn)
+
 
 def mt_sql_tags(output = "Tuples"):
 
@@ -27,15 +28,19 @@ WHERE tg.tag <> ''
 GROUP BY tg.tag
 ORDER BY count(tf.filename_id) DESC, tg.tag;"""
 
-    tagsDf = read_sql(query_str, con = mt_sql_connect())
-    if output == "Tuples":
-        tags = zip(tagsDf['tag'], tagsDf['count'])
-    elif output == "DataFrame":
-        tags = tagsDf.copy()
-    else:
-        tags = None
+    conn = mt_sql_connect()
+
+    with conn.cursor() as curs:
+        curs.execute(query_str)
+        tags = curs.fetchall()
+
+    if output == "DataFrame":
+        tags = DataFrame(tags, columns = ['tag','count'])
+
+    conn.close()
 
     return(tags)
+
 
 def create_tag_list():
     ##### Create tags list
